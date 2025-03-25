0439e2ffa1007c5aa588796eaf60892ca11d5dc1
mari 0 <0somethingsomething0@gmail.com>
2025-03-25 08:47:41 -0400

    Create Day1.py

---
+++ Day1.py
@@ -0,0 +1,45 @@
+import random
+import os as clear
+
+def user_name():
+  name = input("What is your name?: ").title
+  print (f"Welcome {name}")
+  return name
+
+
+def get_user_anwswer():
+  
+
+def get_qna():
+  quizzer = [ 
+    {"question_1": "Who created Spherical Geometry?", "correct":"Bernhard Riemann", "wrong_1":"Jules Ropa", "wrong_2":"Albert Einstein","wrong_3":"John Locke"},
+
+    {"question_2": "Who is credited with the invention of Knot Theory?", "wrong_1": "Muhammad ibn Musa al-Khwarizmi", "wrong_2": "Euclid", "correct": "Sir William Thomson", "wrong_3": "Isaac Newton"},
+
+    {"question_2": "Who created Non-Euclidian Geometry?", "wrong_1": "Leonardo Fibonacci", "correct_1": "Janos Bolyai", "wrong_2": "George Boole", "wrong_3": "Benjamin Franklin"}
+]
+  return quizzer
+
+def r_or_w(quizzer):
+  if user_answer = 
+
+
+
+ # List and Dictionary:
+  math_coran = {
+  "Spherical Geometry": "Bernhard Riemann",
+  "Knot Theory": "Sir William Thomson",
+  "Non-Euclidian Geometry": "Janos Bolyai"
+}
+
+
+
+def qna():
+  quizzer = [ 
+    {"question_1": "Who created Spherical Geometry?", "correct":"Bernhard Riemann", "wrong_1":"Jules Ropa", "wrong_2":"Albert Einstein","wrong_3":"John Locke"},
+
+    {"question_2": "Who is credited with the invention of Knot Theory?", "wrong_1": "Muhammad ibn Musa al-Khwarizmi", "wrong_2": "Euclid", "correct": "Sir William Thomson", "wrong_3": "Isaac Newton"},
+
+    {"question_2": "Who created Non-Euclidian Geometry?", "wrong_1": "Leonardo Fibonacci", "correct_1": "Janos Bolyai", "wrong_2": "George Boole", "wrong_3": "Benjamin Franklin"}
+]
+  return quizzer
